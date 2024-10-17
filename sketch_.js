08f0018e1a9df148904b448d518fe7a4e1930406
camifernandez2003 <camila.fernandez@ucuenca.edu.ec>
2024-10-17 02:27:59 -0500

    Se crearon 100 pelotas rebotando

---
+++ sketch_.js
@@ -0,0 +1,50 @@
+let fonditoDeColorin;
+let pelotas = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  fonditoDeColorin = color(random(10, 25), random(100, 150), random(255));
+  for (let i = 0; i < 100; i++) {
+    let nuevaPelota = new Pelota();
+    pelotas.push(nuevaPelota);
+  }
+}
+
+function draw() {
+  background(fonditoDeColorin);
+  for (let i = 0; i < 100; i++) {
+    pelotas[i].update();
+    pelotas[i].display();
+  }
+}
+
+class Pelota {
+  //únicamente en el objeto pelota
+  constructor() {
+    this.rad = ceil(random(5, 30));
+    this.diam = this.rad * 2;
+    this.posX = random(this.rad, width - this.rad);
+    this.posY = random(this.rad, height - this.rad);
+    this.velX = random(-10, 10);
+    this.velY = random(-10, 10);
+    console.log("estoy viaaaaa!");
+  }
+  // método update
+  update() {
+    this.posX += this.velX;
+    this.posY += this.velY;
+
+    if (this.posX > width - this.rad || this.posX < this.rad) {
+      this.velX *= -1;
+    }
+    if (this.posY > height - this.rad || this.posY < this.rad) {
+      this.velY *= -1;
+    }
+  }
+
+  display() {
+    fill(255);
+    noStroke();
+    circle(this.posX, this.posY, this.diam);
+  }
+}
